d2374a9daf8dab1be8b4ab8432c30db31e5b4719
danieltvaz <danieltostes@live.com>
2023-01-02 15:47:49 -0300

    build: add env check

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import env from "./config/env";
 import express from "express";
 import routes from "./routes";
 
@@ -12,5 +13,5 @@ server.use(bodyParser.json());
 routes(server);
 
 server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port} with db: ${env.DATABASE}`);
 });
