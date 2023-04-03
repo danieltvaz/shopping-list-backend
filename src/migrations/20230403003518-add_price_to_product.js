051e50ca8495d05c90d41581993ff94b69ae1591
danieltvaz <danieltostes@live.com>
2023-04-02 21:38:06 -0300

    chore: add migration

---
+++ src/migrations/20230403003518-add_price_to_product.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Product", "price", Sequelize.DataTypes.STRING);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn("Product", "price");
+  },
+};
