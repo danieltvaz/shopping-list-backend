c7677eeb392b5c75190200cd5d5fbe93662e1a40
danieltvaz <danieltostes@live.com>
2025-06-14 18:27:39 -0300

    chore: remove env from ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,13 +10,6 @@ module.exports = {
       env_file: ".env",
       env: {
         NODE_ENV: "production",
-        API_PORT: process.env.API_PORT,
-        MYSQL_HOST: process.env.MYSQL_HOST,
-        MYSQL_PORT: process.env.MYSQL_PORT,
-        MYSQL_USER: process.env.MYSQL_USER,
-        MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
-        MYSQL_DATABASE: process.env.MYSQL_DATABASE,
-        JWT_SECRET: process.env.JWT_SECRET,
       },
     },
   ],
