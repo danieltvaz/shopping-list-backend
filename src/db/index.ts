a463abc3f49aa7b38efe95f7b699c8fa0bc48ff9
danieltvaz <danieltostes@live.com>
2025-07-06 15:26:39 -0300

    chore: disable sequelize logger

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,6 +5,7 @@ import env from "../config/env";
 const sequelize = new Sequelize(env.DATABASE, env.USERNAME, env.PASSWORD, {
   host: env.HOST,
   dialect: (env.DIALECT as Dialect) ?? "mysql",
+  logging: false,
 });
 
 export default sequelize;
